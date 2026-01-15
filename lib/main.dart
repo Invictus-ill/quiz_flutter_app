13794970cb98e9a93dcda44177cd44ce1fe41f5b
Invictus-ill <77726890+Invictus-ill@users.noreply.github.com>
2026-01-15 08:17:54 +0530

    Add navigation from start page to questions screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_flutter_app/start_screen.dart';
+import 'package:quiz_flutter_app/quiz.dart';
 
 void main() {
-  runApp(MaterialApp(home: const Scaffold(body: StartScreen())));
+  runApp(Quiz());
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_flutter_app/questions_screen.dart';
+import 'package:quiz_flutter_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    super.initState();
+    activeScreen = StartScreen(switchScreen);
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(home: Scaffold(body: activeScreen));
+  }
+}
